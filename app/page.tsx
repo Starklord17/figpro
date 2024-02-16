785debcc00f236efe4d3276f53a8207035ea3ac2
MaxSM <maxi.sm117@gmail.com>
2024-02-16 17:49:45 -0300

    :zap: Update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
-      <h1 className="text-4xl font-bold text-center mt-8 mb-8 text-black md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-display tracking-tight dark:text-white md:tracking-tighter leading-tight">
+    <div className="h-[100vh] w-full flex justify-center items-center text-center">
+      <h1 className="text-4xl font-bold mt-8 mb-8 text-white md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-display tracking-tight dark:text-white md:tracking-tighter leading-tight">
         Liveblocks Figma Clone
       </h1>
+    </div>
   );
 }
