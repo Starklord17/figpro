6f2a997b786d6846176007ee2cb8227e3f57688d
MaxSM <maxi.sm117@gmail.com>
2024-02-16 17:42:46 -0300

    :zap: Add file structure and assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
-
 export default function Page() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+      <h1 className="text-4xl font-bold text-center mt-8 mb-8 text-black md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-display tracking-tight dark:text-white md:tracking-tighter leading-tight">
+        Liveblocks Figma Clone
+      </h1>
   );
 }
