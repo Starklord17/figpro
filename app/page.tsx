6b4f284a638de326244a82d088b5ee6967ebbadb
MaxSM <maxi.sm117@gmail.com>
2024-02-19 19:55:16 -0300

    :zap: Add Sidebars

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
 
 export default function Page() {
   return (
@@ -9,7 +11,11 @@ export default function Page() {
       <Navbar />
 
       <section className="flex h-full flex-row">
+        
+        <LeftSidebar />
         <Live />
+        <RightSidebar />
+
       </section>
     </main>
   );
