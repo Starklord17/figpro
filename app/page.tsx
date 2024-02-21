8905ddb1150b3afa970fe42fbe0d1118d539bcbf
MaxSM <maxi.sm117@gmail.com>
2024-02-21 16:12:24 -0300

    :zap: Add Handle Canvas Mouse Up, Move, Down

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,126 @@
-'use client';
+"use client";
 
-import {fabric} from 'fabric';
+import { fabric } from "fabric";
 
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import LeftSidebar from "@/components/LeftSidebar";
 import RightSidebar from "@/components/RightSidebar";
-import { useEffect, useRef, useState } from 'react';
-import { handleCanvasMouseDown, handleResize, initializeFabric } from '@/lib/canvas';
-import { ActiveElement } from '@/types/type';
+import { useEffect, useRef, useState } from "react";
+import {
+  handleCanvasMouseDown,
+  handleCanvasMouseMove,
+  handleCanvasMouseUp,
+  handleResize,
+  initializeFabric,
+} from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
+import { useMutation, useStorage } from "@/liveblocks.config";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const fabricRef = useRef<fabric.Canvas | null>(null);
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
-  const selectedShapeRef = useRef<string | null>('rectangle');
+  const selectedShapeRef = useRef<string | null>("rectangle");
+  const activeObjectRef = useRef<fabric.Object | null>(null);
+
+  const canvasObjects = useStorage((root) => root.canvasObjects);
+
+  const syncShapeInStorage = useMutation(({ storage }, object) => {
+    if (!object) return;
+
+    const { objectId } = object;
+
+    const shapeData = object.toJSON();
+    shapeData.objectId = objectId;
+
+    const canvasObjects = storage.get("canvasObjects");
+
+    canvasObjects.set(objectId, shapeData);
+  }, []);
 
   const [activeElement, setActiveElement] = useState<ActiveElement>({
-    name: '',
-    value: '',
-    icon: '',
-  })
+    name: "",
+    value: "",
+    icon: "",
+  });
 
   const handleActiveElement = (elem: ActiveElement) => {
     setActiveElement(elem);
 
     selectedShapeRef.current = elem?.value as string;
-  }
+  };
 
   useEffect(() => {
-    const canvas = initializeFabric({canvasRef, fabricRef});
+    // initialize the fabric canvas
+    const canvas = initializeFabric({ canvasRef, fabricRef });
 
+    /**
+     * Listen to the mouse down event on the canvas which is fired when the
+     * user clicks on the canvas.
+     *
+     * Event inspector: http://fabricjs.com/events
+     * Event list: http://fabricjs.com/docs/fabric.Canvas.html#fire
+      */
     canvas.on("mouse:down", (options) => {
       handleCanvasMouseDown({
         options,
         canvas,
         isDrawing,
         shapeRef,
-        selectedShapeRef
-      })
+        selectedShapeRef,
+      });
+    });
 
-    })
+    
+    /**
+     * Listen to the mouse move event on the canvas which is fired when the
+     * user moves the mouse on the canvas.
+     */
+    canvas.on("mouse:move", (options) => {
+      handleCanvasMouseMove({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+      });
+    });
+
+    /**
+     * Listen to the mouse up event on the canvas which is fired when the
+     * user releases the mouse on the canvas.
+     */
+    canvas.on("mouse:up", () => {
+      handleCanvasMouseUp({
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+        setActiveElement,
+        activeObjectRef,
+      });
+    });
 
     window.addEventListener("resize", () => {
-      handleResize({fabricRef})
-    })
-
-  }, [])
+      handleResize({ fabricRef });
+    });
+  }, []);
 
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar 
+      <Navbar
         activeElement={activeElement}
         handleActiveElement={handleActiveElement}
       />
 
       <section className="flex h-full flex-row">
-        
         <LeftSidebar />
         <Live canvasRef={canvasRef} />
         <RightSidebar />
-
       </section>
     </main>
   );
