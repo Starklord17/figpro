f88c4091e72cdb12ebbe617b13219ec0bbe43197
MaxSM <maxi.sm117@gmail.com>
2024-02-21 17:03:10 -0300

    :zap: Delete and reset feature

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,8 @@ import {
 } from "@/lib/canvas";
 import { ActiveElement } from "@/types/type";
 import { useMutation, useStorage } from "@/liveblocks.config";
+import { defaultNavElement } from "@/constants";
+import { handleDelete } from "@/lib/key-events";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -48,10 +50,109 @@ export default function Page() {
     icon: "",
   });
 
+  /**
+   * DeleteAllShapes is a mutation that deletes all the shapes from the
+   * key-value store of liveblocks.
+   *
+   * delete: https://liveblocks.io/docs/api-reference/liveblocks-client#LiveMap.delete
+   * get: https://liveblocks.io/docs/api-reference/liveblocks-client#LiveMap.get
+   *
+   * We're using this mutation to delete all the shapes from the key-value store when the user clicks on the reset button.
+   */
+  const deleteAllShapes = useMutation(({ storage }) => {
+    // get the canvasObjects store
+    const canvasObjects = storage.get("canvasObjects");
+
+    // if the store doesn't exist or is empty, return
+    if (!canvasObjects || canvasObjects.size === 0) return true;
+
+    // delete all the shapes from the store
+    for (const [key, value] of canvasObjects.entries()) {
+      canvasObjects.delete(key);
+    }
+
+    // return true if the store is empty
+    return canvasObjects.size === 0;
+  }, []);
+
+   /**
+   * deleteShapeFromStorage is a mutation that deletes a shape from the
+   * key-value store of liveblocks.
+   * useMutation is a hook provided by Liveblocks that allows you to perform
+   * mutations on liveblocks data.
+   *
+   * useMutation: https://liveblocks.io/docs/api-reference/liveblocks-react#useMutation
+   * delete: https://liveblocks.io/docs/api-reference/liveblocks-client#LiveMap.delete
+   * get: https://liveblocks.io/docs/api-reference/liveblocks-client#LiveMap.get
+   *
+   * We're using this mutation to delete a shape from the key-value store when
+   * the user deletes a shape from the canvas.
+   */
+   const deleteShapeFromStorage = useMutation(({ storage }, shapeId) => {
+    /**
+     * canvasObjects is a Map that contains all the shapes in the key-value.
+     * Like a store. We can create multiple stores in liveblocks.
+     *
+     * delete: https://liveblocks.io/docs/api-reference/liveblocks-client#LiveMap.delete
+     */
+    const canvasObjects = storage.get("canvasObjects");
+    canvasObjects.delete(shapeId);
+  }, []);
+
+  /**
+   * Set the active element in the navbar and perform the action based
+   * on the selected element.
+   *
+   * @param elem
+   */
   const handleActiveElement = (elem: ActiveElement) => {
     setActiveElement(elem);
 
-    selectedShapeRef.current = elem?.value as string;
+    switch (elem?.value) {
+      // delete all the shapes from the canvas
+      case "reset":
+        // clear the storage
+        deleteAllShapes();
+        // clear the canvas
+        fabricRef.current?.clear();
+        // set "select" as the active element
+        setActiveElement(defaultNavElement);
+        break;
+
+      // delete the selected shape from the canvas
+      case "delete":
+        // delete it from the canvas
+        handleDelete(fabricRef.current as any, deleteShapeFromStorage);
+        // set "select" as the active element
+        setActiveElement(defaultNavElement);
+        break;
+
+      // upload an image to the canvas
+      case "image":
+        // trigger the click event on the input element which opens the file dialog
+        imageInputRef.current?.click();
+        /**
+         * set drawing mode to false
+         * If the user is drawing on the canvas, we want to stop the
+         * drawing mode when clicked on the image item from the dropdown.
+         */
+        isDrawing.current = false;
+
+        if (fabricRef.current) {
+          // disable the drawing mode of canvas
+          fabricRef.current.isDrawingMode = false;
+        }
+        break;
+
+      // for comments, do nothing
+      case "comments":
+        break;
+
+      default:
+        // set the selected shape to the selected element
+        selectedShapeRef.current = elem?.value as string;
+        break;
+    }
   };
 
   useEffect(() => {
@@ -121,7 +222,11 @@ export default function Page() {
 
     window.addEventListener("resize", () => {
       handleResize({ fabricRef });
-    });
+    })
+
+    return () => {
+      canvas.dispose();
+    }
   }, []);
 
   useEffect(() => {
