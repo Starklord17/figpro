44cc3b95266985f38de76bc20686bdeee12be702
MaxSM <maxi.sm117@gmail.com>
2024-02-23 16:03:08 -0300

    :zap: Update Avatar

--- components/users/Avatar.tsx
+++ components/users/Avatar.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import styles from "./Avatar.module.css";
 import Image from "next/image";
+
+import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from "../ui/tooltip";
 
 /**
  * This file shows how to add live avatars like you can see them at the top right of a Google Doc or a Figma file.
@@ -12,19 +12,46 @@ import Image from "next/image";
  * See pages/api/liveblocks-auth.ts and https://liveblocks.io/docs/api-reference/liveblocks-node#authorize for more information
  */
 
-const IMAGE_SIZE = 48;
+// const IMAGE_SIZE = 48;
 
-export function Avatar({ name, otherStyles }: { otherStyles: string; name: string }) {
-  return (
-    <div className={`${styles.avatar} ${otherStyles} h-9 w-9`} data-tooltip={name}>
-      <Image
-        src={`https://liveblocks.io/avatars/avatar-${Math.floor(Math.random() * 30)}.png`}
-        fill
-        className={styles.avatar_picture}
-        alt={name}
-      />
-    </div>
-  );
-}
+// export function Avatar({ name, otherStyles }: { otherStyles: string; name: string }) {
+//   return (
+//     <div className={`${styles.avatar} ${otherStyles} h-9 w-9`} data-tooltip={name}>
+//       <Image
+//         src={`https://liveblocks.io/avatars/avatar-${Math.floor(Math.random() * 30)}.png`}
+//         fill
+//         className={styles.avatar_picture}
+//         alt={name}
+//       />
+//     </div>
+//   );
+// }
+
+type Props = {
+  name: string;
+  otherStyles?: string;
+};
+
+const Avatar = ({ name, otherStyles }: Props) => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <div className={`relative h-9 w-9 rounded-full ${otherStyles}`} data-tooltip={name}>
+          <Image
+            src={`https://liveblocks.io/avatars/avatar-${Math.floor(Math.random() * 30)}.png`}
+            fill
+            className="rounded-full"
+            alt={name}
+          />
+        </div>
+      </TooltipTrigger>
+      <TooltipContent className="border-none bg-primary-grey-200 px-2.5 py-1.5 text-xs">
+        {name}
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
+
+export default Avatar;
 
 // Source: https://liveblocks.io/examples/live-avatar-stack/nextjs-live-avatars
