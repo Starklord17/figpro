02b938b1530bc9ad69986ec685cc0319817c0c53
MaxSM <maxi.sm117@gmail.com>
2024-02-23 02:34:06 -0300

    :zap: Room

--- app/Room.tsx
+++ app/Room.tsx
@@ -8,12 +8,31 @@ import Loader from "@/components/Loader";
 
 export function Room({ children }: { children: ReactNode }) {
   return (
-    <RoomProvider id="my-room" 
+    <RoomProvider
+      id="my-room"
+      /**
+       * initialPresence is used to initialize the presence of the current
+       * user in the room.
+       *
+       * initialPresence: https://liveblocks.io/docs/api-reference/liveblocks-react#RoomProvider
+       */
       initialPresence={{
-        cursor: null, cursorCollor: null, editingText: null
+        cursor: null,
+        cursorColor: null,
+        editingText: null,
       }}
+      /**
+       * initialStorage is used to initialize the storage of the room.
+       *
+       * initialStorage: https://liveblocks.io/docs/api-reference/liveblocks-react#RoomProvider
+       */
       initialStorage={{
-        canvasObjects: new LiveMap()
+        /**
+         * LiveMap is used to store the canvas objects
+         *
+         * LiveMap: https://liveblocks.io/docs/api-reference/liveblocks-client#LiveMap
+         */
+        canvasObjects: new LiveMap(),
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>
